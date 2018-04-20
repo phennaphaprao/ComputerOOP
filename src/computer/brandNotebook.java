c6c5dc9a367cc5efe17559fe73760384e4079de9
Janjira012 <s5830213012@phuket.psu.ac.th>
2018-04-21 05:01:54 +0700

    update

--- src/computer/brandNotebook.java
+++ src/computer/brandNotebook.java
@@ -55,70 +55,70 @@ public class brandNotebook extends generation{
         if(this.q1==1&&super.asus==1){
             System.out.println("Notebook brand >>> "+this.brand1);
             System.out.println("ASUS generation >>> Asus E402NA-GA239T ");
-            System.out.println("Salary is "+super.Asus1+" THB");
+            System.out.println("price is "+super.Asus1+" THB");
         }else if(this.q1==1&&super.asus==2){
             System.out.println("Notebook brand >>> "+this.brand1);
             System.out.println("ASUS generation >>> Asus X555QG-XX157T ");
-            System.out.println("Salary is "+super.Asus2+" THB");
+            System.out.println("price is "+super.Asus2+" THB");
         }else if(this.q1==1&&super.asus==3){
             System.out.println("Notebook brand >>> "+this.brand1);
             System.out.println("ASUS generation >>> Asus K550IK-GO017T ");
-            System.out.println("Salary is "+super.Asus3+" THB");
+            System.out.println("price is "+super.Asus3+" THB");
         }else if(this.q1==1&&super.asus==4){
             System.out.println("Notebook brand >>> "+this.brand1);
             System.out.println("ASUS generation >>> Asus Zenbook UX370UA-C4178T Touch ");
-            System.out.println("Salary is "+super.Asus4+" THB");
+            System.out.println("price is "+super.Asus4+" THB");
         }
         else if(this.q1==2&&super.asus==1){
             System.out.println("Notebook brand >>> "+this.brand2);
             System.out.println("ACER generation >>> Acer Aspire E5-474G-56YN ");
-            System.out.println("Salary is "+super.Acer1+" THB");
+            System.out.println("price is "+super.Acer1+" THB");
         }else if(this.q1==2&&super.asus==2){
             System.out.println("Notebook brand >>> "+this.brand2);
             System.out.println("ACER generation >>> Acer Aspire E5-571-372Z ");
-            System.out.println("Salary is "+super.Acer2+" THB");
+            System.out.println("price is "+super.Acer2+" THB");
         }else if(this.q1==2&&super.asus==3){
             System.out.println("Notebook brand >>> "+this.brand2);
             System.out.println("ACER generation >>> Acer Aspire E5-573G-52WX ");
-            System.out.println("Salary is "+super.Acer3+" THB");
+            System.out.println("price is "+super.Acer3+" THB");
         }else if(this.q1==2&&super.asus==4){
             System.out.println("Notebook brand >>> "+this.brand2);
             System.out.println("ACER generation >>> Acer Aspire E5-572G-78HY ");
-            System.out.println("Salary is "+super.Acer4+" THB");
+            System.out.println("price is "+super.Acer4+" THB");
         }
         else if(this.q1==3&&super.asus==1){
             System.out.println("Notebook brand >>> "+this.brand3);
             System.out.println("DELL generation >>> Dell Inspiron 11-3147 ");
-            System.out.println("Salary is "+super.Dell1+" THB");
+            System.out.println("price is "+super.Dell1+" THB");
         }else if(this.q1==3&&super.asus==2){
             System.out.println("Notebook brand >>> "+this.brand3);
             System.out.println("DELL generation >>> Dell Inspiron 15-5547 ");
-            System.out.println("Salary is "+super.Dell2+" THB");
+            System.out.println("price is "+super.Dell2+" THB");
         }else if(this.q1==3&&super.asus==3){
             System.out.println("Notebook brand >>> "+this.brand3);
             System.out.println("DELL generation >>> Dell XPS 13-L321X ");
-            System.out.println("Salary is "+super.Dell3+" THB");
+            System.out.println("price is "+super.Dell3+" THB");
         }else if(this.q1==3&&super.asus==4){
             System.out.println("Notebook brand >>> "+this.brand3);
             System.out.println("DELL generation >>> Dell Inspiron 15-7548 ");
-            System.out.println("Salary is "+super.Dell4+" THB");
+            System.out.println("price is "+super.Dell4+" THB");
         }
         else if(this.q1==4&&super.asus==1){
             System.out.println("Notebook brand >>> "+this.brand4);
             System.out.println("MSI generation >>> MSI GL62M 7RDX-1496XTH ");
-            System.out.println("Salary is "+super.Msi1+" THB");
+            System.out.println("price is "+super.Msi1+" THB");
         }else if(this.q1==4&&super.asus==2){
             System.out.println("Notebook brand >>> "+this.brand4);
             System.out.println("MSI generation >>> MSI GE60 2PC APACHE ");
-            System.out.println("Salary is "+super.Msi2+" THB");
+            System.out.println("price is "+super.Msi2+" THB");
         }else if(this.q1==4&&super.asus==3){
             System.out.println("Notebook brand >>> "+this.brand4);
             System.out.println("MSI generation >>> MSI GL62M 7RDX-1248XTH ");
-            System.out.println("Salary is "+super.Msi3+" THB");
+            System.out.println("price is "+super.Msi3+" THB");
         }else if(this.q1==4&&super.asus==4){
             System.out.println("Notebook brand >>> "+this.brand4);
             System.out.println("MSI generation >>> MSI GL62M 7REX ");
-            System.out.println("Salary is "+super.Msi4+" THB");
+            System.out.println("price is "+super.Msi4+" THB");
         }
   
     }
